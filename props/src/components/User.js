18ad14e23b82de624e4c82f362c72ff9cfb06d12
Özgür Kalkan <ahmetozgurkalkan@gmail.com>
2022-08-04 21:28:02 +0300

    propTypes defaulProps

--- props/src/components/User.js
+++ props/src/components/User.js
@@ -1,11 +1,12 @@
 import propTypes from "prop-types";
 
 function User({ name, surName, isLoggedIn, age, friends, adress }) {
+	if (!isLoggedIn) {
+		return <div>Giriş Yapmadınız</div>;
+	}
 	return (
 		<>
-			<h1>
-				{isLoggedIn ? `${name} ${surName} yaşınız ${age}` : "Giriş Yapmadınız"}
-			</h1>
+			<h1>{`${name} ${surName} yaşınız ${age}`}</h1>
 			{adress.title} {adress.zip}
 			{friends &&
 				friends.map((friend) => <div key={friend.id}>{friend.name}</div>)}
@@ -15,10 +16,14 @@ function User({ name, surName, isLoggedIn, age, friends, adress }) {
 User.propTypes = {
 	name: propTypes.string.isRequired,
 	surName: propTypes.string.isRequired,
-	isLoggedIn: propTypes.bool.isRequired,
+	isLoggedIn: propTypes.bool,
 	age: propTypes.oneOfType([propTypes.number, propTypes.string]).isRequired,
 	friends: propTypes.array,
 	adress: propTypes.shape({ title: propTypes.string, zip: propTypes.string }),
+};
+
+User.defaultProps = {
+	isLoggedIn: false,
 };
 
 export default User;
