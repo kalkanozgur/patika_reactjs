4775faa933b7d8c86b38f07ee83386bf07d3fd94
Özgür Kalkan <ahmetozgurkalkan@gmail.com>
2022-08-04 22:27:43 +0300

    array states

--- states/src/App.js
+++ states/src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 function App() {
 	const [name, setName] = useState("Özgür");
 	const [age, setAge] = useState(30);
+	const [friends, setFriends] = useState(["Ahmet", "Cem"]);
 	return (
 		<div className="App">
 			<h1>Merhaba {name}</h1>
@@ -11,6 +12,15 @@ function App() {
 			<button onClick={() => setName("Ahmet Özgür")}>Change name </button>
 			<br />
 			<button onClick={() => setAge(31)}>Change age </button>
+			<hr />
+			<h2>Friends</h2>
+			{friends.map((friend, index) => (
+				<div key={index}>{friend} </div>
+			))}
+			<br />
+			<button onClick={() => setFriends([...friends, "Düşün"])}>
+				Add new friend{" "}
+			</button>
 		</div>
 	);
 }
