fa40561ac5548c5bf061baae74b6835c16c1c951
Özgür Kalkan <ahmetozgurkalkan@gmail.com>
2022-08-04 18:07:41 +0300

    Using objects in props

--- props/src/App.js
+++ props/src/App.js
@@ -1,12 +1,49 @@
 import "./App.css";
 import User from "./components/User";
 
+const friends = [
+	{
+		id: 1,
+		name: "Ahmet",
+	},
+	{
+		id: 2,
+		name: "Salih",
+	},
+	{
+		id: 3,
+		name: "Cem",
+	},
+	{
+		id: 4,
+		name: "Düşün",
+	},
+	{
+		id: 5,
+		name: "Onur",
+	},
+	{
+		id: 6,
+		name: "Işıl",
+	},
+	{
+		id: 7,
+		name: "Kutay",
+	},
+];
+
 function App() {
-  return (
-    <div className="App">
-      <User name={"Özgür"} surName={"KALKAN"} isLoggedIn={true} age={30} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<User
+				name={"Özgür"}
+				surName={"KALKAN"}
+				isLoggedIn={true}
+				age={30}
+				friends={friends}
+			/>
+		</div>
+	);
 }
 
 export default App;
--- props/src/components/User.js
+++ props/src/components/User.js
@@ -1,9 +1,14 @@
-function User({ name, surName, isLoggedIn, age }) {
-  return (
-    <h1>
-      {isLoggedIn ? `${name} ${surName} yaşınız ${age}` : "Giriş Yapmadınız"}
-    </h1>
-  );
+function User({ name, surName, isLoggedIn, age, friends }) {
+	return (
+		<>
+			<h1>
+				{isLoggedIn ? `${name} ${surName} yaşınız ${age}` : "Giriş Yapmadınız"}
+			</h1>
+			{friends.map((friend) => (
+				<div key={friend.id}>{friend.name}</div>
+			))}
+		</>
+	);
 }
 
 export default User;
