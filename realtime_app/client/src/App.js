885dcbf310db62f0e688fa4f278cd73be3d95aff
Özgür Kalkan <ahmetozgurkalkan@gmail.com>
2022-08-14 21:40:57 +0300

    socket.io-client init()

--- realtime_app/client/src/App.js
+++ realtime_app/client/src/App.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
+
 import "./App.css";
 import Palette from "./components/Palette";
 
+import { init } from "./SocketApi";
+
 function App() {
+	useEffect(() => {
+		init();
+	}, []);
 	return (
 		<div className="App">
 			<Palette />
